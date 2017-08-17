c246c0eb22c51b2a45c5585fe68710f48f0089b6
danielrmartin <danielraymartin@gmail.com>
2017-08-17 13:16:43 -0500

    Update for JW 2017

---
+++ vars/common.groovy
@@ -0,0 +1,49 @@
+def runPreDeploymentTests(serviceName, registry) {
+    stage "pre-deployment tests"
+    def dir = pwd()
+    sh "mkdir -p ${dir}/db"
+    sh "chmod 0777 ${dir}/db"
+      sh "docker run --rm -v $dir/training-books-ms/target/scala-2.10:/source/target/scala-2.10 -v db:/data/db docker-registry:5000 ./run_tests.sh"
+}
+
+def build(serviceName, registry) {
+    stage "build"
+      sh "docker build -t docker-registry:5000/books-ms ."
+                    sh "docker push docker-registry:5000/books-ms"
+                    stash includes: "docker-compose*.yml", name: "docker-compose"
+    stash includes: "docker-compose*.yml", name: "docker-compose"
+}
+
+def deploy(serviceName, registry) {
+    node("production") {
+        stage "deploy"
+        def response = input message: 'Please confirm deployment to production', ok: 'Submit', parameters: [[$class: 'StringParameterDefinition', defaultValue: '', description: 'Additional comments', name: '']], submitter: 'manager'
+        echo response
+        unstash "docker-compose"
+        def pull = [:]
+        pull["service"] = {
+            docker.image("${registry}/${serviceName}").pull()
+        }
+        pull["db"] = {
+            docker.image("mongo").pull()
+        }
+        parallel pull
+        sh "docker-compose -p books-ms up -d app"
+        sleep 2
+    }
+}
+
+def runPostDeploymentTests(serviceName, registry, domain) {
+    stage "post-deployment tests"{
+    def tests = docker.image("${registry}/${serviceName}-tests")
+    tests.inside() {
+        withEnv(["TEST_TYPE=integ", "DOMAIN=${domain}"]) {
+            retry(2) {
+                sh "./run_tests.sh"
+            }
+        }
+        }
+    }
+}
+
+return this;
