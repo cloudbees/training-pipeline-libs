e5f0a2924def84edf8dba844493217aabf2b92c5
danielrmartin <danielraymartin@gmail.com>
2017-08-18 08:44:47 -0500

    Update common.groovy
    Remove stages

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,22 +1,24 @@
 def runPreDeploymentTests(serviceName, registry) {
-    stage "pre-deployment tests"
+//    stage ("pre-deployment tests"){
     def dir = pwd()
     sh "mkdir -p ${dir}/db"
     sh "chmod 0777 ${dir}/db"
       sh "docker run --rm -v $dir/target/scala-2.10:/source/target/scala-2.10 -v db:/data/db ${registry}/${serviceName}-tests ./run_tests.sh"
-}
+    }
+//}
 
 def build(serviceName, registry) {
-    stage "build"
+   // stage ("build"){
       sh "docker build -t ${registry}/books-ms ."
                     sh "docker push ${registry}0/books-ms"
                     stash includes: "docker-compose*.yml", name: "docker-compose"
     stash includes: "docker-compose*.yml", name: "docker-compose"
+//    }
 }
 
 def deploy(serviceName, registry) {
     node("production") {
-        stage "deploy"
+     //   stage ("deploy"){
         def response = input message: 'Please confirm deployment to production', ok: 'Submit', parameters: [[$class: 'StringParameterDefinition', defaultValue: '', description: 'Additional comments', name: '']], submitter: 'manager'
         echo response
         unstash "docker-compose"
@@ -31,10 +33,11 @@ def deploy(serviceName, registry) {
         sh "docker-compose -p books-ms up -d app"
         sleep 2
     }
+   // }
 }
 
 def runPostDeploymentTests(serviceName, registry, domain) {
-    stage "post-deployment tests"{
+ //   stage ("post-deployment tests"){
     def tests = docker.image("${registry}/${serviceName}-tests")
     tests.inside() {
         withEnv(["TEST_TYPE=integ", "DOMAIN=${domain}"]) {
@@ -45,5 +48,6 @@ def runPostDeploymentTests(serviceName, registry, domain) {
         }
     }
 }
+//}
 
 return this;
